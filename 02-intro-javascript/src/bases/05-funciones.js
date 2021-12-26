203e79c3c675dd684c4270ae63f71e6b903f694c
Jeronimo Deiros <jeronimodeiros@gmail.com>
2021-12-26 19:38:24 -0300

    Funciones

---
+++ 02-intro-javascript/src/bases/05-funciones.js
@@ -0,0 +1,58 @@
+// Funciones en JS
+const saludar = function saludar( nombre ){
+    return `Hola ${nombre}`;
+}
+// lo que pasa es que si solo coloco function saludar 
+// corro el riesgo de pisar mas adelante con una variable 
+// que se llame saludar a lo que es la function
+// entonces declaro la const saludar y le asigno la funcion
+
+//funcion de flecha
+const saludar2 = ( nombre ) => {
+    return `Hola ${nombre}`;
+}
+//una notacion mas simple
+const saludar3 = ( nombre ) =>  `Hola ${nombre}`;
+
+console.log(saludar('Marisa'));
+console.log(saludar2('Agustin'));
+console.log(saludar3('Roberto'));
+
+const getUser = () => {
+    return {
+        uid: 'BEE123',
+        username: 'jbDragon'
+    }
+}
+
+console.log(getUser());
+
+//pero ojo, veamos este caso, las {} pueden generar error, 
+//al ser confundidos con el cuerpo de la funcion en lugar de una coleccion
+//entonces, lo que hago es envolver lo que devuelvo entre ():
+const getUser2 = () => ({
+        uid: 'FH1212',
+        username: 'Fantasma'
+    })
+
+console.log(getUser2());
+
+const saludar4 = () => (`Hola Mundo`);
+console.log(saludar4());
+
+// Tarea
+// 1. Transformen a una funcion de Flecha
+// 2. Tiene que retornar un objeto implícito
+// 3. Pruebas
+// function getUsuarioActivo( nombre ){
+//     return {
+//         uid: 'ABC567',
+//         username: nombre
+//     }
+// }
+const getUsuarioActivo = ( nombre ) =>
+    ({
+        uid: 'ABC567',
+        username: nombre
+    });
+console.log(getUsuarioActivo('Jerónimo'));
