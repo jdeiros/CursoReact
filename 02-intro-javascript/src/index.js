1dae251359ffa627fedd63b212cb15f5756bc836
Jerónimo Deiros <jeronimo.deiros@ey.com>
2022-01-16 13:12:34 -0300

    update from work

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,4 @@
+// fetch API
+// https://developers.giphy.com/
+// https://developer.mozilla.org/es/docs/Web/API/Fetch_API
+// json viewer awesome -> complemento del chrome para ver json formateado.
