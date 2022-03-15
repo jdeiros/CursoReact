85a3e1e6d5f8ffe46f6a23e2ccf10ee120a8cef3
Jeronimo Deiros <jeronimodeiros@gmail.com>
2022-03-14 21:44:30 -0300

    add category

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+
+export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    // const handleAdd = () => {
+    //     setCategories(categs => [...categs, 'HunterXHunter']);
+    //     console.log(categories);
+    // }
+    return (
+    <>
+        <h2>GifExpertApp</h2>
+        <AddCategory />
+        <hr/>
+
+        <ol>
+            {
+                categories.map(category =>{
+                    return <li key={category}> { category} </li>
+                })
+            }
+        </ol>
+    </>
+  )
+}
