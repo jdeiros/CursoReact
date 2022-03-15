122b65055e74597fcbdabddf805efc026d8ec16e
Jerónimo Deiros <jeronimo.deiros@ey.com>
2022-03-15 17:07:01 -0300

    custom hooks

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,23 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { getGifs } from '../helpers/getGifs';
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [count, setCount] = useState([]);
-
-    const [images, setImages] = useState([]);
-
-    useEffect(() => {
-        getGifs(category)
-            .then(imgs => setImages(imgs));
-    }, [category]); // [category] hace que si la category cambia va a volver a ejecutar este efecto
-
-
+    const { data: images, loading } = useFetchGifs(category);
 
     return (
         <>
             <h3> {category} </h3>
+
+            {loading && <p>Loading</p>}
+
             <div className="card-grid">
                 {
                     images.map(img => (
